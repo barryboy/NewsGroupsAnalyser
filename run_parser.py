84c30553bbb07d2e55fcd6a4e863ffc73ae89859
barryboy <ziembowicz@gmail.com>
2013-11-14 23:36:07 +0100

    redirected output to a .csv file

--- run_parser.py
+++ run_parser.py
@@ -7,10 +7,15 @@ A runner for Parser class from nntp_parser.py
 import nntp_parser 
 import sys
 
-if len(sys.argv) < 2:
-    sys.exit('No argument given.\nSTOPPING.\n')
+largv = len(sys.argv)
 
-nntp_parser= nntp_parser.Parser(sys.argv[1])
+if largv < 2:
+    sys.exit('No argument given.\nSTOPPING.\n')
+infile = sys.argv[1]
+splitted = infile.split('/') 
+outfile = splitted[len(splitted)-1] + '.csv'
+
+nntp_parser= nntp_parser.Parser(infile)
 nntp_parser.parse()
 nntp_parser.populateThreadTags()
 nntp_parser.anonimizeUsers()
@@ -18,21 +23,32 @@ nntp_parser.getTail()
 nntp_parser.parseTails()
 dictionary = nntp_parser.getParsedDict()
 
-
-print 'THREAD\tID\tAUTHOR\tREF\tABA\tABC\tABAB\tABAC\tABCA\tABCB\tABCD\tTAIL'
+f = open(outfile, 'w')
+f.write('THREAD\tID\tAUTHOR\tREF\ttail_length\tABA\tABC\tABAB\tABAC\tABCA\tABCB\tABCD\tnABA\tnABC\tnABAB\tnABAC\tnABCA\tnABCB\tnABCD\tTAIL\n')
 for ID in dictionary.keys():
     msg = dictionary.get(ID)
-    if 'tag' in msg:
-        print str(msg.get('tag')) + '\t',
-        print str(msg.get('id')) + '\t',
-        print str(msg.get('author')) + '\t',
-        #print str(msg.get('date')) + '\t',
-        print str(msg.get('references')) + '\t',
-        print str(msg.get('ABA')) + '\t',
-        print str(msg.get('ABC')) + '\t',
-        print str(msg.get('ABAB')) + '\t',
-        print str(msg.get('ABAC')) + '\t',
-        print str(msg.get('ABCA')) + '\t',
-        print str(msg.get('ABCB')) + '\t',
-        print str(msg.get('ABCD')) + '\t',
-        print str(msg.get('tail'))
+    line = ''
+    line += str(msg.get('tag')) + '\t'
+    line += str(msg.get('id')) + '\t'
+    line += str(msg.get('author')) + '\t'
+    #line += str(msg.get('date')) + '\t'
+    line += str(msg.get('references')) + '\t'
+    line += str(msg.get('tail_length')) + '\t'
+    line += str(msg.get('ABA')) + '\t'
+    line += str(msg.get('ABC')) + '\t'
+    line += str(msg.get('ABAB')) + '\t'
+    line += str(msg.get('ABAC')) + '\t'
+    line += str(msg.get('ABCA')) + '\t'
+    line += str(msg.get('ABCB')) + '\t'
+    line += str(msg.get('ABCD')) + '\t'
+    line += str(msg.get('nABA')) + '\t'
+    line += str(msg.get('nABC')) + '\t'
+    line += str(msg.get('nABAB')) + '\t'
+    line += str(msg.get('nABAC')) + '\t'
+    line += str(msg.get('nABCA')) + '\t'
+    line += str(msg.get('nABCB')) + '\t'
+    line += str(msg.get('nABCD')) + '\t'
+    line += str(msg.get('tail'))
+    f.write(line + '\n')
+
+f.close()
